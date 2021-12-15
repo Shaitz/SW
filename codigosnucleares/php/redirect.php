4f3ae8b4ab09bb781598a3edee33934b234c3c8f
EHU <a@a.com>
2021-12-15 10:35:02 +0100

    login social

--- codigosnucleares/php/redirect.php
+++ codigosnucleares/php/redirect.php
@@ -1,10 +1,10 @@
 <?php
 require_once '../../vendor/autoload.php';
-  
+session_start();
 // init configuration
 $clientID = '384084108129-rblosilev8dr7mekip69paaednbdt3o2.apps.googleusercontent.com';
 $clientSecret = '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';
-$redirectUri = 'Layout.php';
+$redirectUri = 'http://localhost/sw/codigosnucleares/php/redirect.php';
    
 // create Client Request to access Google API
 $client = new Google_Client();
@@ -15,15 +15,22 @@ $client->addScope("email");
 $client->addScope("profile");
   
 // authenticate code from Google OAuth Flow
-if (isset($_GET['code'])) {
+if (isset($_GET['code'])) 
+{
   $token = $client->fetchAccessTokenWithAuthCode($_GET['code']);
   $client->setAccessToken($token['access_token']);
-   
+  
   // get profile info
   $google_oauth = new Google_Service_Oauth2($client);
   $google_account_info = $google_oauth->userinfo->get();
-  $email =  $google_account_info->email;
-  $name =  $google_account_info->name;
+  $email = $google_account_info->email;
+  $name = $google_account_info->name;
+  $picture = $google_account_info->picture;
+  
+  $_SESSION['user'] = $email;
+  $_SESSION['rol'] = 'Estudiante';
+  $_SESSION['foto'] = $picture;
+  header("Location: Layout.php");
   
   // now you can use this profile info to create account in your website and make user logged in.
 } else {
